a560f6b1d4e6d192178632741ef47660bb777c1a
vishnupriya2919 <vishnu.nov2919@gmail.com>
2021-09-06 18:31:42 +0530

    commited

---
+++ src/org/simple/Simple.java
@@ -0,0 +1,27 @@
+package org.simple;
+
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Simple {
+	public static void main(String[] args) {
+		java.util.List li=new LinkedList();
+		li.add(10);
+		li.add(20);
+		li.add(30);
+		li.add(40);
+		li.add(50);
+		li.add(60);
+		
+		
+		
+		System.out.println(li);
+		
+		int de=li.indexOf(2);
+		System.out.println(de);
+		
+		
+	}
+
+}
